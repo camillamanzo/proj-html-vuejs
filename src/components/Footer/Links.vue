87e262b448c28477429367f65044422dd953f139
camillamanzo <camillasouthafrica@gmail.com>
2021-10-08 10:43:26 +0200

    initialized style in footer  links

--- src/components/Footer/Links.vue
+++ src/components/Footer/Links.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="fluid-container">
-        <div class="row">
+    <div class="fluid-container" id="links">
+        <div class="row d-flex justify-content-center">
             <div class="col-2" id="contact-info">
-                <img src="../../assets/images/classic_shop_logo_footer.png" alt="avada logo">
+                <img src="../../assets/images/classic_shop_logo_footer.png" alt="avada logo" class="logo-img">
                 <div v-for="x in contact" :key="x">
                     <p>{{ x }}</p>
                 </div>
             </div>
             <div class="col-2" id="top-rated">
                 <h6>Top rated products</h6>
-                <div v-for="y in topRated" :key="y" class="d-flex">
+                <div v-for="y in topRated" :key="y" class="d-flex justify-content-between">
                     <div>
                         <p>{{ y.name }}</p>
                         <span><i v-for="q in 5" :key="q" class="fas fa-star"></i></span>
                         <p>{{ y.price }}</p>
                     </div>
                     
-                    <img :src="y.thumbs" :alt="y.name">
+                    <img :src="y.thumbs" :alt="y.name" class="thumbs">
                 </div>
             </div>
             <div class="col-2" id="recent-post">
@@ -29,7 +29,7 @@
             <div class="col-2" id="tags">
                 <h6>tags</h6>
                 <div class="my_tag" v-for="el in tags" :key="el">
-                    <div>{{ el }}</div>
+                    <span>{{ el }}</span>
                 </div>
             </div>
         </div>
@@ -76,5 +76,18 @@ export default {
 <style scoped lang="scss">
 @import "../../style/variables.scss";
 @import "../../style/general.scss";
+
+#links{
+    background-color: $footer-bg-md;
+
+    .logo-img{
+        width: 180px;
+    }
+
+    .thumbs{
+        width: 50px;
+        height: 100%;
+    }
+}
 
 </style>
